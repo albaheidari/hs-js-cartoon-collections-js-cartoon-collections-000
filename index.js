e0b9941615c357243d0d2ef31ea1fc638dc62de7
Alba Heidari <albaheidari@gmail.com>
2018-10-10 15:02:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
+ let enanos = [];
+  for(let i = 0; i < dwarves.length;i++){
+     enanos.push((i + 1) + ". "+  dwarves[i]);         
+  }
+   return enanos.join(' ');
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
+  const capitan =[] ;
+  for( let i = 0; i < planeteerCalls.length;i++){
+    capitan.push(planeteerCalls[i].toUpperCase() + "!");
+  }return capitan;
 }
 
 function longPlaneteerCalls(words) {
+  let lista = true;
+  if (words.length > 4) {
+    return lista;
+} else {
+    return false;
+  }
 }
 
 function findTheCheese (foods) {
+  let quesos =['cheddar','gouda','camembert'];
+  for(var i = 0; i < foods.length; i++){
+    for(var j = 0; j < quesos.length; j++){
+      if(quesos[j] === foods[i]){
+        return foods[i];
+        
+      }
+    }
+  }
+  return 'no cheese!';
+
 }
